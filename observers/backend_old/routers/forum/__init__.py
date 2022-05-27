4b6a3bf6e6e889b743540626521d60a6559a94fc
Gipss <gissa.dmtr@gmail.com>
2022-05-27 19:47:53 +0300

    Deleted old dirs

--- observers/backend_old/routers/forum/__init__.py
+++
@@ -1,9 +0,0 @@
-from fastapi import APIRouter
-
-from . import questions, tags, comments
-
-router = APIRouter(prefix='/forum')
-
-router.include_router(questions.router)
-router.include_router(tags.router)
-router.include_router(comments.router)
--- observers/backend_old/routers/news/__init__.py
+++
@@ -1,7 +0,0 @@
-from fastapi import APIRouter
-
-from . import articles
-
-router = APIRouter(prefix='/news')
-
-router.include_router(articles.router)
